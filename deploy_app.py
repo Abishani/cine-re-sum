706c10e2a02560e4a75e4ba0aef64f62ed8ab236
Abishani Muthalagan <abishani.2019743@iit.ac.lk>
2023-05-22 01:13:20 +0530

    added files for app deployment and development

--- deploy_app.py
+++ deploy_app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import requests
-import secrets_1 as py_secrets
 
-#=========================== Load Pegasus tokenizer and model =============================
+# =========================== Load Pegasus tokenizer and model =============================
 tokenizer = AutoTokenizer.from_pretrained(
-    "Abishani/amrs-cineresum-summarizer", use_auth_token=py_secrets.access_token
+    "Abishani/amrs-cineresum-summarizer", use_auth_token=st.secrets['access_token']
 )
 model = AutoModelForSeq2SeqLM.from_pretrained(
-    "Abishani/amrs-cineresum-summarizer", use_auth_token=py_secrets.access_token
+    "Abishani/amrs-cineresum-summarizer", use_auth_token=st.secrets['access_token']
 )
 max_length = 100
 min_length = 80
@@ -31,7 +30,7 @@ def pegasus_summarize(text):
 
 # ======================================= API call ================================= #
 def API_call(movie_name):
-    API_KEY = py_secrets.api_key
+    API_KEY = st.secrets['api_key']
 
     # Search for the movie using the SearchMovie endpoint
     search_url = f'https://imdb-api.com/en/API/SearchMovie/{API_KEY}/{movie_name}'
