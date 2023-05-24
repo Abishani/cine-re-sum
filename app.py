930d29eb951798e251ae677a5afdad4c3a71219c
Abishani Muthalagan <abishani.2019743@iit.ac.lk>
2023-05-24 17:03:44 +0530

    added imdb redirect link, add number of reviews from the response, deleted the deploy_app.py and changed the server headless=false

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import requests
 import secrets_1 as py_secrets
+import webbrowser
 
-#=========================== Load Pegasus tokenizer and model =============================
+# =========================== Load Pegasus tokenizer and model =============================
 tokenizer = AutoTokenizer.from_pretrained(
     "Abishani/amrs-cineresum-summarizer", use_auth_token=py_secrets.access_token
 )
@@ -37,39 +38,68 @@ def API_call(movie_name):
     search_url = f'https://imdb-api.com/en/API/SearchMovie/{API_KEY}/{movie_name}'
     search_response = requests.get(search_url).json()
 
-    # Get the ID of the first search result
+    # Check if any search results are found
     if search_response['errorMessage']:
         print(search_response['errorMessage'])
+        st.write("Movie is not found. Please enter a different movie.")
     else:
         search_results = search_response['results']
         if search_results:
             movie_id = search_results[0]['id']
+         # Check if the exact movie is found in the search results
+         # exact_movie_found = False
+         # for result in search_results:
+         #     if movie_name.lower() in result['title'].lower():
+         #         exact_movie_found = True
+         #         movie_id = result['id']
+         #         break
 
+         # if exact_movie_found:
             # Retrieve the movie reviews using the Reviews endpoint
             reviews_url = f'https://imdb-api.com/en/API/Reviews/{API_KEY}/{movie_id}'
             reviews_response = requests.get(reviews_url).json()
 
-            # get all the reviews (['items'][i]['content']) and combine it into one string
-            reviews = [item["content"] for item in reviews_response["items"]]
-            review_text = "\n".join(reviews)
+            if reviews_response['errorMessage']:
+                print(reviews_response['errorMessage'])
+                st.write("No reviews found for this movie.")
+            else:
+                # Get all the reviews (['items'][i]['content']) and combine them into one string
+                reviews = [item["content"]
+                           for item in reviews_response["items"]]
 
-            # print the movie name from the api call
-            st.title(search_results[0]['title'])
+                # Print the movie name from the API call
+                st.title(search_results[0]['title'])
 
-            summary = pegasus_summarize(review_text)
-            st.write("Summary: ")
-            st.write(summary)
+                # Print the count of total reviews
+                st.write("Number of Total Reviews:", len(reviews))
 
-            # print the movie image from the api call
-            st.image(search_results[0]['image'])
+                # If "See All Reviews" button is clicked it will redirect to IMDB website
+                def open_imdb_website():
+                    imdb_link = "https://www.imdb.com/"
+                    webbrowser.open(imdb_link)
 
+                st.button("See All Reviews!", on_click=open_imdb_website)
+
+                summary = pegasus_summarize("\n".join(reviews))
+                st.write("Summary:")
+                st.write(summary)
+
+                # Display the movie image if available, else display "Image not found"
+                if search_results[0]['image']:
+                    st.image(search_results[0]['image'])
+                else:
+                    st.write("Image not found")
+         # else:
+         #     print('Movie not found in the search results.')
+         #     st.write("Movie is not found. Please enter a different movie.")
         else:
             print('No search results found.')
+            st.write("Movie is not found. Please enter a different movie.")
 
 
 # =============================== Display UI ============================
 st.title("CineReSum 📽️")
-movie_name = st.text_area("Enter the movie name 👇", height=100)
+movie_name = st.text_input("Enter the movie name 👇")
 
 
 # ========================== summarize button ==========================
